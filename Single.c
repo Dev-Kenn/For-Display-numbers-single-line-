d1ce66e2e9af164046f0b87b03eaa6d1d5c5cf1b
Mark Kenneth Ferrer <81341766+Dev-Kenn@users.noreply.github.com>
2021-05-02 12:23:34 +0800

    Add files via upload

---
+++ Single.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define p printf
+#define s scanf
+
+/*MArk Kenneth O. Ferrer DICT-1-3 */
+
+main()
+{
+    int i;
+	p ("Display numbers from 0 to 100 in a single line:	\n\n");
+	for (i = 100; i >= 0; i--)
+	
+        {
+        printf("%d ", i);
+        }
+ return 0;
+}
